ddc1a7ec825a543c65271a572d9bbd60f9c722eb
n3tsploit <netsploit@tutanota.com>
2022-11-17 17:17:41 +0300

    updates

--- telebot/app.py
+++ telebot/app.py
@@ -1,7 +1,31 @@
-import json
 import telegram
 from telegram.ext import *
 from telegram import *
 from telebot import functions
 from dotenv import load_dotenv
+from pathlib import Path
 import os
+
+load_dotenv(Path("./telebot/.env"))
+TOKEN = os.getenv('TOKEN')
+
+
+def start_command():
+    print('hello')
+
+
+def messages():
+    print('me')
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    disp = updater.dispatcher
+
+    disp.add_handler(CommandHandler('help', start_command))
+    disp.add_handler(CommandHandler('start', start_command))
+    disp.add_handler(CommandHandler('check', start_command))
+
+    disp.add_handler(MessageHandler(Filters.text, messages))
+
+    updater.start_polling()
