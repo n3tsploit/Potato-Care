1bc373e69f44f19a227bd13d2582030206ace10f
n3tsploit <netsploit@tutanota.com>
2022-12-19 23:23:44 +0300

    updates

--- create_model.py
+++ create_model.py
@@ -3,30 +3,26 @@ from tensorflow.keras import models, layers
 import matplotlib.pyplot as plt
 
 # load data into tf.dataset
-image_size = 256
-batch_size = 32
-channels = 3
-dataset = tf.keras.preprocessing.image_dataset_from_directory("dataset", shuffle=True,
-                                                              image_size=(image_size, image_size),
-                                                              batch_size=batch_size)
+image_size=256
+batch_size=32
+channels=3
+dataset = tf.keras.preprocessing.image_dataset_from_directory("dataset",shuffle=True,image_size=(image_size,image_size), batch_size=batch_size)
 class_names = dataset.class_names
-
 
 # split dataset
 def get_dataset_partition_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
-    ds_size = len(ds)
-    if shuffle:
-        ds = ds.shuffle(shuffle_size, seed=12)
+  ds_size=len(ds)
+  if shuffle:
+    ds = ds.shuffle(shuffle_size, seed=12)
 
-    train_size = int(train_split * ds_size)
-    val_size = int(val_split * ds_size)
+  train_size=int(train_split*ds_size)
+  val_size= int(val_split* ds_size)
 
-    train_ds = ds.take(train_size)
-    val_ds = ds.skip(train_size).take(val_size)
-    test_ds = ds.skip(train_size).skip(val_size)
+  train_ds = ds.take(train_size)
+  val_ds = ds.skip(train_size).take(val_size)
+  test_ds = ds.skip(train_size).skip(val_size)
 
-    return train_ds, val_ds, test_ds
-
+  return train_ds, val_ds, test_ds
 
 train_ds, val_ds, test_ds = get_dataset_partition_tf(dataset)
 
@@ -35,4 +31,7 @@ train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
 val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
 test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
 
-# data augmentation
+# preprocessing
+resize_and_rescale = tf.keras.Sequential([layers.experimental.preprocessing.Resizing(image_size,image_size), layers.experimental.preprocessing.Rescaling(1.0/255)])
+data_augmentation = tf.keras.Sequential([layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"), layers.experimental.preprocessing.RandomRotation(0.2)])
+
