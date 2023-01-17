e4f31064e8264f4c693ad9ed07da8b3a02e2002b
n3tsploit <netsploit@tutanota.com>
2023-01-17 10:36:38 +0300

    updates

--- telebot/create_model.py
+++ telebot/create_model.py
@@ -3,26 +3,30 @@ from tensorflow.keras import models, layers
 import matplotlib.pyplot as plt
 
 # load data into tf.dataset
-image_size=256
-batch_size=32
-channels=3
-dataset = tf.keras.preprocessing.image_dataset_from_directory("dataset",shuffle=True,image_size=(image_size, image_size), batch_size=batch_size)
+image_size = 256
+batch_size = 32
+channels = 3
+dataset = tf.keras.preprocessing.image_dataset_from_directory("dataset", shuffle=True,
+                                                              image_size=(image_size, image_size),
+                                                              batch_size=batch_size)
 class_names = dataset.class_names
+
 
 # split dataset
 def get_dataset_partition_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
-  ds_size=len(ds)
-  if shuffle:
-    ds = ds.shuffle(shuffle_size, seed=12)
+    ds_size = len(ds)
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
 
-  train_size=int(train_split*ds_size)
-  val_size= int(val_split* ds_size)
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
 
-  train_ds = ds.take(train_size)
-  val_ds = ds.skip(train_size).take(val_size)
-  test_ds = ds.skip(train_size).skip(val_size)
+    train_ds = ds.take(train_size)
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
 
-  return train_ds, val_ds, test_ds
+    return train_ds, val_ds, test_ds
+
 
 train_ds, val_ds, test_ds = get_dataset_partition_tf(dataset)
 
@@ -32,8 +36,10 @@ val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
 test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
 
 # preprocessing
-resize_and_rescale = tf.keras.Sequential([layers.experimental.preprocessing.Resizing(image_size,image_size), layers.experimental.preprocessing.Rescaling(1.0/255)])
-data_augmentation = tf.keras.Sequential([layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"), layers.experimental.preprocessing.RandomRotation(0.2)])
+resize_and_rescale = tf.keras.Sequential([layers.experimental.preprocessing.Resizing(image_size, image_size),
+                                          layers.experimental.preprocessing.Rescaling(1.0 / 255)])
+data_augmentation = tf.keras.Sequential([layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+                                         layers.experimental.preprocessing.RandomRotation(0.2)])
 
 # building model
 input_shape = (batch_size, image_size, image_size, channels)
@@ -41,11 +47,11 @@ n_classes = 3
 
 model = models.Sequential([
     resize_and_rescale,
-    layers.Conv2D(32, kernel_size = (3,3), activation='relu', input_shape=input_shape),
+    layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
@@ -73,4 +79,4 @@ history = model.fit(
     epochs=50,
 )
 
-model.save("lpotatoes.h5")
+model.save("potatoes.h5")
